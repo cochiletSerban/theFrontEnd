c5e192709653c06e78f0e95132eeebfa7ce9dbc5
cochiletSerban <cochiletSernam@gmail.com>
2019-05-17 13:15:48 +0300

    added feed

--- src/app/pages/feed/feed.component.ts
+++ src/app/pages/feed/feed.component.ts
@@ -1,3 +1,5 @@
+import { Image } from './../../models/image';
+import { ImageService } from './../../services/image-service.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./feed.component.scss']
 })
 export class FeedComponent implements OnInit {
+  feedImages: Image[];
 
-  constructor() { }
+  constructor(private imageService: ImageService) { }
 
   ngOnInit() {
+    this.imageService.getLandingPageBgs().subscribe(res => this.feedImages = res);
   }
+
 
 }
