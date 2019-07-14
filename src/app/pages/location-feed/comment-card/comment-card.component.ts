aa0b80e85091c2eaeb7db629e9b3a3276800809d
cochiletSerban <cochiletSerban@gmail.com>
2019-07-14 18:55:41 +0300

    feat [COMMENTCARD]: style comment card a bit more

--- src/app/pages/location-feed/comment-card/comment-card.component.ts
+++ src/app/pages/location-feed/comment-card/comment-card.component.ts
@@ -1,16 +1,27 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, AfterViewInit, ElementRef, Renderer2 } from '@angular/core';
+
 
 @Component({
   selector: 'app-comment-card',
   templateUrl: './comment-card.component.html',
   styleUrls: ['./comment-card.component.scss']
 })
-export class CommentCardComponent implements OnInit {
+export class CommentCardComponent implements OnInit, AfterViewInit {
   @Input() comment: Comment;
-  constructor() { }
+  isTouch = false;
+  constructor(private renderer: Renderer2, elRef: ElementRef) { }
 
   ngOnInit() {
 
   }
+
+  ngAfterViewInit() {
+  // //  this.isTouch = (('ontouchstart' in window) || (navigator.msMaxTouchPoints > 0));
+    
+  }
+
+  // onHover(element) {
+  //   this.renderer.setStyle(element.navtive)
+  // }
 
 }
