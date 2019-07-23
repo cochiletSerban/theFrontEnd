fd592faff8f42af449348e901ec01a0ba6be9886
cochiletSerban <cochiletSerban@gmail.com>
2019-07-23 20:42:52 +0300

    fix [MOBILE][LOCATIONFEED][SCROLL]: fixed scroll bar showing on mobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,11 @@ import { Component, OnInit } from '@angular/core';
 
 export class AppComponent implements OnInit {
   constructor() {}
-  ngOnInit() {}
+  ngOnInit() {
+    window.addEventListener('resize', () => {
+      // We execute the same script as before
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    });
+  }
 }
