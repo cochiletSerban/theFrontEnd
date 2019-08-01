4aa8e4e85c1b5eee6451939718233cae2fb8def6
cochiletSerban <cochiletSernam@gmail.com>
2019-08-01 16:20:39 +0300

    feat [LOCATIONFEED][MAP]: user cand now select to upload photos form the map

--- src/app/pages/location-feed/map/map.component.ts
+++ src/app/pages/location-feed/map/map.component.ts
@@ -4,19 +4,21 @@ import { LatLngBounds } from '@agm/core';
 import { LocationService } from 'src/app/services/location.service';
 import { Location } from '../../../models/location';
 import { Image } from 'src/app/models/image';
-import { Component, OnInit, Input, } from '@angular/core';
+import { Component, OnInit, Input, Renderer2, AfterViewInit, ElementRef } from '@angular/core';
 import { isAbsolute } from 'path';
 import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
 import { Subject } from 'rxjs';
 import * as _ from 'lodash';
 import { DeviceDetectorService } from 'ngx-device-detector';
+import { stringify } from '@angular/core/src/util';
 declare const google: any;
+import * as M from 'materialize-css';
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss']
 })
-export class MapComponent implements OnInit {
+export class MapComponent implements OnInit, AfterViewInit {
 
   // public darkStyle = [
   //   {
@@ -168,25 +170,26 @@ export class MapComponent implements OnInit {
 
   @Input() initialImages: Image[];
   @Input() userLocation: Location;
-
-
   mapCenter;
   bottomEdge;
   topEdge;
   map;
   zoom;
-
+  uploadMarker;
+  locationUploadModal;
+  confirmationModal;
   debug = false;
-
+  canAddMarkers = false;
   boundsChanged: Subject<Location> = new Subject<Location>();
   picturesOnMap: Image[] = [];
   isMobile: boolean;
+  showFinalUploadStep = false;
 
   constructor(private locationService: LocationService, private imageService: ImageService,
-              private userService: UserService, private deviceService: DeviceDetectorService) {
-    this.boundsChanged.pipe(
-        debounceTime(300),
-        distinctUntilChanged(),
+              private userService: UserService, private deviceService: DeviceDetectorService,
+              private renderer: Renderer2, private elRef: ElementRef
+              ) {
+    this.boundsChanged.pipe( debounceTime(300), distinctUntilChanged(),
         switchMap((location => this.imageService.getImagesInMyArea(location)))
       ).subscribe(images => {
         this.picturesOnMap.push(...images);
@@ -201,6 +204,26 @@ export class MapComponent implements OnInit {
     this.mapCenter = this.userLocation;
     this.picturesOnMap = this.initialImages;
     this.zoom = this.calculateZoom(window.innerWidth, 100, this.userLocation.lat, 1);
+  }
+
+  ngAfterViewInit() {
+    const buttons = document.querySelectorAll('.fixed-action-btn');
+    const instances = M.FloatingActionButton.init(buttons, {
+      direction: 'up',
+      hoverEnabled: false
+    });
+
+    let modal = document.querySelector('#locationUploadModal');
+    this.locationUploadModal = M.Modal.init(modal);
+
+    modal = document.querySelector('#confirmationModal');
+    this.confirmationModal = M.Modal.init(modal);
+
+  }
+
+  mapReady(map) {
+    this.map = map;
+    this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(document.getElementById('hud'));
   }
 
   calculateZoom(WidthPixel, Ratio, Lat, Length) {
@@ -230,13 +253,58 @@ export class MapComponent implements OnInit {
     this.mapCenter.lon = event.lng;
   }
 
-  mapReady(map) {
-    this.map = map;
-    this.map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(document.getElementById('goToUserLocation'));
-    this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(document.getElementById('hud'));
+  addPoi(ev) {
+    if (this.canAddMarkers) {
+      this.uploadMarker = new google.maps.Marker({
+        position: new google.maps.LatLng(+ev.lat, +ev.lng),
+        map: this.map,
+        draggable: true
+      });
+      this.canAddMarkers = false;
+    }
   }
 
   goToUserLocation() {
       this.map.setCenter({ lat: this.locationService.getCurrentLocation().lat, lng: this.locationService.getCurrentLocation().lon });
+      // console.log(JSON.parse(JSON.stringify(this.uploadMarker.position)));
   }
+
+  enableMarkers() {
+    this.canAddMarkers = true;
+    this.renderer.setStyle(this.elRef.nativeElement.querySelector('agm-map'), 'cursor', 'pointer');
+  }
+
+  uploadPicture() { // 1
+    this.locationUploadModal.open();
+  }
+
+  goToUpload(type?) { // 4
+    if (type) {
+      console.log(JSON.parse(JSON.stringify(this.uploadMarker.position)));
+      this.cancelUpload();
+    } else {
+      console.log(this.userLocation);
+    }
+    this.showFinalUploadStep = false;
+  }
+
+  goToMarkerUpload() { // 3
+    this.canAddMarkers = true;  // ?
+    this.showFinalUploadStep = true;
+  }
+
+  confirm() { // 2
+    this.confirmationModal.open();
+  }
+
+  cancelUpload() { // 4
+    if (this.uploadMarker) {
+      this.canAddMarkers = false;
+      this.uploadMarker.setMap(null);
+      this.uploadMarker = null;
+    }
+    this.showFinalUploadStep = false;
+
+  }
+
 }
