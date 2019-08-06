1d477dcd0e1cbb4471d44bb37067895db1b8a8de
cochiletSerban <cochiletSernam@gmail.com>
2019-08-06 17:29:23 +0300

    WIP added dymanic forms for upload

--- src/app/pages/picture-upload/edit-info/edit-info.component.ts
+++ src/app/pages/picture-upload/edit-info/edit-info.component.ts
@@ -1,6 +1,7 @@
 import { Image } from './../../../models/image';
 import { ImageUploadService } from './../../../services/image-upload.service';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-info',
@@ -9,16 +10,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditInfoComponent implements OnInit {
 
-  constructor(private imageUploadService: ImageUploadService) { }
+  constructor(private imageUploadService: ImageUploadService, private fb: FormBuilder) {}
 
-  uploader;
+  name = 'Angular';
+  form: FormGroup;
+  arr: FormArray;
+
   images: Image[] = [];
   loading = true;
+  uploader;
 
   ngOnInit() {
+    this.form = this.fb.group({
+      arr: this.fb.array([])
+    });
+
     this.uploader = this.imageUploadService.getUploader();
-    this.imageUploadService.getUploaderImages().subscribe(res => this.images = res)
-      .add(() => this.loading = false);
+
+    this.imageUploadService.getUploaderImages().subscribe(res => {
+      this.images = res;
+      this.images.forEach(img => {
+        this.addItem();
+      });
+    }).add(() => this.loading = false);
+
+  }
+
+
+
+
+    // this.imageUploadService.getUploaderImages().subscribe(res => {
+
+    //   this.images = res;
+
+
+
+    // }).add(() => this.loading = false);
 
 
     // this.uploader.onCompleteItem = (item, response, status) => {
@@ -26,29 +53,27 @@ export class EditInfoComponent implements OnInit {
     // };
 
     // this.uploader.uploadAll();
+
+
+
+
+
+  createItem() {
+    return this.fb.group({
+      description: [''],
+      title: ['']
+    });
+  }
+
+  addItem() {
+    this.arr = this.form.get('arr') as FormArray;
+    this.arr.push(this.createItem());
+  }
+
+  onSubmit() {
+    console.log(this.form.value);
   }
 
 
-  // getUploaderImages() {
-  //   return new Promise((resolve, reject) => {
-
-  //     const images: Image[] = [];
-  //     this.uploader.queue.forEach((file) => {
-  //       const fileReader = new FileReader();
-  //       fileReader.readAsDataURL(file._file);
-  //       fileReader.onloadend = (e) => {
-  //         images.push({
-  //           fileName: 'yolo',
-  //           picture: String(fileReader.result)
-  //         });
-
-  //         if (images.length === this.uploader.queue.length) {
-  //           resolve(images);
-  //         }
-
-  //       };
-  //     });
-  //   });
-  // }
 
 }
