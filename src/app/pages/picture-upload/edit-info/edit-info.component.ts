f329b244d9706403062f4f4a84166c37f8d2d560
cochiletSerban <cochiletSernam@gmail.com>
2019-08-20 13:07:16 +0300

    feat [UPLOAD]: upload now works

--- src/app/pages/picture-upload/edit-info/edit-info.component.ts
+++ src/app/pages/picture-upload/edit-info/edit-info.component.ts
@@ -2,7 +2,7 @@ import { switchMap, tap } from 'rxjs/operators';
 import { Image } from './../../../models/image';
 import { ImageUploadService } from './../../../services/image-upload.service';
 import { Component, OnInit, ViewChild, ElementRef, NgZone, AfterViewInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { MapsAPILoader } from '@agm/core';
 import { LocationService } from 'src/app/services/location.service';
 declare var M: any;
@@ -16,14 +16,15 @@ export class EditInfoComponent implements OnInit, AfterViewInit {
   @ViewChild('search')  searchElementRef;
 
   form: FormGroup = new FormGroup({
-    title: new FormControl(null),
+    title: new FormControl(null, Validators.required),
     description: new FormControl(null),
     tags: new FormControl(null),
-    location: new FormControl(null),
+    location: new FormControl(null, Validators.required),
   });
 
   images: Image[] = [];
-  loading = true;
+  loadingPicture = true;
+  loadingAddress = true;
   uploader;
   geoCoder;
   lat;
@@ -32,17 +33,12 @@ export class EditInfoComponent implements OnInit, AfterViewInit {
   uploadMarker;
   map;
 
-  // iau locatia pt harta si geocode
-  // locatie este fie locationService.getCurrentLocation() fie this.imageUploadService.getImageLocation()
-  // daca getImageLocation nu e null iau locatia din serviciu creez markerul si geocodeul
-  // else fac acelasi lucru dar cu getCurrent location
+
 
   constructor(private imageUploadService: ImageUploadService, private locationService: LocationService,
               private mapsAPILoader: MapsAPILoader, private ngZone: NgZone) {
 
     this.zoom = this.locationService.calculateZoom(window.innerWidth, 100, this.locationService.getCurrentLocation().lat, 1);
-    // this.lat = this.locationService.getCurrentLocation().lat;
-    // this.lon = this.locationService.getCurrentLocation().lon;
 
     if (this.imageUploadService.getImageLocation()) {
       this.locationService.getAddressFromLocation(this.imageUploadService.getImageLocation())
@@ -52,10 +48,10 @@ export class EditInfoComponent implements OnInit, AfterViewInit {
     } else {
       const location = {
         lat: this.locationService.getCurrentLocation().lat,
-        lon: this.locationService.getCurrentLocation().lon
+        lng: this.locationService.getCurrentLocation().lon
       };
       this.locationService.getAddressFromLocation(location).subscribe(address => {
-        this.createMarkerAndPopulateAddress(this.imageUploadService.getImageLocation(), address);
+        this.createMarkerAndPopulateAddress(location, address);
       });
     }
 
@@ -63,20 +59,27 @@ export class EditInfoComponent implements OnInit, AfterViewInit {
 
  createMarkerAndPopulateAddress(location, address) {
   this.lat = location.lat;
-  this.lon = location.lon;
+  this.lon = location.lon || location.lng;
   this.addPoi({lat: this.lat, lng: this.lon});
   this.uploadMarker.addListener('dragend', this.handleMarkerDrag.bind(this));
   this.form.get('location').patchValue(address);
   this.form.updateValueAndValidity();
   M.updateTextFields();
+  this.loadingAddress = false;
+  this.map.setCenter({ lat: this.lat, lng: this.lon });
  }
 
   ngOnInit() {
     this.uploader = this.imageUploadService.getUploader();
     this.imageUploadService.getUploaderImages().subscribe(res => {
       this.images = res;
-    }).add(() => this.loading = false);
+    }).add(() => this.loadingPicture = false);
 
+    this.uploader.onCompleteItem = (item, response, status) => {
+      this.loadingPicture = false;
+      this.loadingAddress = false;
+      console.log('ImageUpload:uploaded:', item, status, response);
+    };
 
   }
 
@@ -124,30 +127,21 @@ export class EditInfoComponent implements OnInit, AfterViewInit {
 
 
 
-    // this.imageUploadService.getUploaderImages().subscribe(res => {
-
-    //   this.images = res;
-
-
-
-    // }).add(() => this.loading = false);
-
-
-    // this.uploader.onCompleteItem = (item, response, status) => {
-    //   console.log('ImageUpload:uploaded:', item, status, response);
-    // };
-
-    // this.uploader.uploadAll();
-
-
-
-
-
-
-
   onSubmit() {
-    console.log(this.form.value);
-  }
+    if (this.form.valid) {
+      this.loadingPicture = true;
+      this.loadingAddress = true;
+      this.uploader.options.additionalParameter = {
+        title: this.form.get('title').value,
+        // tags: this.form.get('tags').value,
+        description: this.form.get('description').value,
+        private: false,
+        lat: this.lat,
+        lon: this.lon
+      };
+      this.uploader.uploadAll();
+    }
+   }
 
   addPoi(ev) {
     this.uploadMarker = new google.maps.Marker({
